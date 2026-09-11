b840f3aaabe868739c7cadc18ed9ad6372d02919
devanshpratap84-sketch <devanshpratap84@gmail.com>
2026-09-11 09:30:16 +0530

    Create app.py
    creating app.py

---
+++ app.py
@@ -0,0 +1,68 @@
+from flask import Flask, request, jsonify
+import joblib
+import pandas as pd
+import os
+
+app = Flask(__name__)
+
+MODEL_PATH = os.path.join(
+    os.path.dirname(__file__),
+    "models",
+    "best_fraud_model.joblib"
+)
+
+# Load trained model
+model_data = joblib.load(MODEL_PATH)
+
+model = model_data["model"]
+threshold = model_data["threshold"]
+feature_columns = model_data["feature_columns"]
+
+
+@app.route("/", methods=["GET"])
+def home():
+    return jsonify({
+        "message": "Fraud Detection API is running!",
+        "model": "Credit Card Fraud Detection",
+        "threshold": threshold
+    })
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+
+    try:
+        data = request.get_json()
+
+        # Create dataframe from received data
+        transaction = pd.DataFrame([data])
+
+        # Ensure correct feature order
+        transaction = transaction[feature_columns]
+
+        # Get fraud probability
+        probability = model.predict_proba(transaction)[0][1]
+
+        # Fraud prediction
+        prediction = int(probability >= threshold)
+
+        # Risk level
+        if probability < 0.20:
+            risk = "Low"
+        elif probability < 0.50:
+            risk = "Medium"
+        elif probability < 0.80:
+            risk = "High"
+        else:
+            risk = "Critical"
+
+        return jsonify({
+            "fraud_probability": round(float(probability), 4),
+            "prediction": prediction,
+            "risk_level": risk
+        })
+
+    except Exception as e:
+        return jsonify({
+            "error": str(e)
+        }), 400
